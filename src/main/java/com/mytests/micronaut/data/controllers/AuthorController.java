caa999d820981447d69618fb8fd492e4ba439b38
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-08-26 22:31:23 +0300

    first repository test (query methods plus controller and http client to test these queries)

---
+++ src/main/java/com/mytests/micronaut/data/controllers/AuthorController.java
@@ -0,0 +1,43 @@
+package com.mytests.micronaut.data.controllers;
+
+import com.mytests.micronaut.data.Author;
+import com.mytests.micronaut.repos.AuthorRepo;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+import jakarta.inject.Inject;
+
+/**
+ * *
+ * <p>Created by irina on 26.08.2021.</p>
+ * <p>Project: micronaut-r2dbc-test</p>
+ * *
+ */
+@Controller
+public class AuthorController {
+    @Inject
+    AuthorRepo authorRepo;
+    @Get("/allAuthors")
+    public Flowable<Author> allAuthors() {
+        return Flowable.fromPublisher(authorRepo.findAll());
+    }
+
+    @Get("/authorsByNick/{name}")
+    public Flowable<Author> authorsByNick(@PathVariable String name)
+
+    {
+        return Flowable.fromPublisher(authorRepo.findByNickName(name));
+    }
+
+    @Get("/authorByName/{name}")
+    public Flowable<Author> authorByName(@PathVariable String name) {
+        return Flowable.fromPublisher(authorRepo.findByFullNameContains((name)));
+    }
+    @Get("/authorById/{id}")
+    public Single<Author> authorById(@PathVariable String id) {
+       
+        return Single.fromPublisher(authorRepo.findById(Integer.parseInt(id)));
+    }
+}
--- src/main/java/com/mytests/micronaut/repos/AuthorRepo.java
+++ src/main/java/com/mytests/micronaut/repos/AuthorRepo.java
@@ -4,6 +4,12 @@ import com.mytests.micronaut.data.Author;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository;
 import io.micronaut.data.repository.reactive.ReactiveStreamsCrudRepository;
+import io.reactivex.Single;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * *
@@ -14,5 +20,8 @@ import io.micronaut.data.repository.reactive.ReactiveStreamsCrudRepository;
 @R2dbcRepository(dialect = Dialect.H2)
 public interface AuthorRepo extends ReactiveStreamsCrudRepository<Author, Integer> {
 
+   Flux<Author> findByNickName(String nickName);
 
+   
+   Publisher<Author> findByFullNameContains(String fullName);
 }
